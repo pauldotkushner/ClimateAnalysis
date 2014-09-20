a9ca304c55fe128df443725851a70470dbffbd61
Paul Kushner <paul.kushner@utoronto.ca>
2014-09-20 16:29:59 -0400

    updates to test_netcdf_create.py

--- test_netcdf_create.py
+++ test_netcdf_create.py
@@ -2,17 +2,24 @@
 #Paul Kushner
 #See netCDF4 documentation at http://unidata.github.io/netcdf4-python/
 import netCDF4
+#basic numerical processing in python
 import numpy
+#open file for writing, use NETCDF3 for compatibility
 file=netCDF4.Dataset('test_dataset.nc','w',format='NETCDF3_CLASSIC')
+#create x and y dimensions
 xdim = file.createDimension('x',3)
 ydim = file.createDimension('y',4)
 xval = file.createVariable('x','f8',('x'))
 yval = file.createVariable('y','f8',('y'))
+#follow c indexing conventions
 data = file.createVariable('data','f8',('y','x'))
+#some descriptive information is output.
 print 'shape xval, yval, data',numpy.shape(xval),numpy.shape(yval),numpy.shape(data)
+#fill in data values
 xval[:]=[1,2,3]
 yval[:]=[-2,0,2,4]
 data[:,:]=[[20,19,18],[17,16,15],[14,13,12],[11,10,9]]
+#closing the file writes it
 file.close()
 #shell commands to test the dataset
 #ncdump test_dataset.nc
