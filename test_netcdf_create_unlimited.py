68c3b81aa6ab2993bdc4dbebdaf0aeb324948e49
Paul Kushner <paul.kushner@utoronto.ca>
2014-09-20 17:49:59 -0400

    unlimited - temperature

--- test_netcdf_create_unlimited.py
+++ test_netcdf_create_unlimited.py
@@ -1,30 +1,25 @@
-#test_netcdf: create a netcdf file with bogus data
+#test_netcdf: create a netcdf file with data representing hypothetical temperature measurements
 #Paul Kushner
 #See netCDF4 documentation at http://unidata.github.io/netcdf4-python/
 import netCDF4
+#basic numerical processing in python
 import numpy
-file=netCDF4.Dataset('test_dataset_unlimited.nc','w',format='NETCDF3_CLASSIC')
+#open file for writing, use NETCDF3 for compatibility
+file=netCDF4.Dataset('temperature_time_record.nc','w',format='NETCDF3_CLASSIC')
 xdim = file.createDimension('x',3)
 ydim = file.createDimension('y',4)
 tdim = file.createDimension('t', None)
-xval = file.createVariable('x','f8',('x'))
-yval = file.createVariable('y','f8',('y'))
+xval = file.createVariable('x','f4',('x'))
+yval = file.createVariable('y','f4',('y'))
+#often, time is indicated as a double precision variable
 tval = file.createVariable('t','f8',('t'))
-data = file.createVariable('data','f8',('t','y','x'))
-print 'shape xval, yval, data',numpy.shape(xval),numpy.shape(yval),numpy.shape(data)
+temperature = file.createVariable('temperature','f8',('t','y','x'))
+print 'shape xval, yval, temperature',numpy.shape(xval),numpy.shape(yval),numpy.shape(temperature)
 xval[:]=[1,2,3]
 yval[:]=[-2,0,2,4]
 tval[:] = [0.1,0.2,0.3]
-data[0,:,:]=[[20,19,18],[17,16,15],[14,13,12],[11,10,9]]
-data[1,:,:]=data[0,:,:]*1.1
-data[2,:,:]=data[1,:,:]*1.1
-print 'shape xval, yval, data',numpy.shape(xval),numpy.shape(yval),numpy.shape(data)
+temperature[0,:,:]=[[20,19,18],[17,16,15],[14,13,12],[11,10,9]]
+temperature[1,:,:]=temperature[0,:,:]*1.1
+temperature[2,:,:]=temperature[1,:,:]*1.1
+print 'shape xval, yval, temperature',numpy.shape(xval),numpy.shape(yval),numpy.shape(temperature)
 file.close()
-#shell commands to test the dataset
-#python test_netcdf_create_unlimited.py
-#ncdump test_dataset.nc
-#ncdump -c test_dataset.nc
-#ncdump -h test_dataset.nc
-#ncwa -a x test_dataset.nc xave.nc
-#ncdump xave.nc
-#ncview test_dataset.nc
