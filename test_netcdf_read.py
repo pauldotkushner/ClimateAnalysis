11694a586158d8b3c4b763de842bead8010ba1d8
Paul Kushner <paul.kushner@utoronto.ca>
2014-09-21 07:41:13 -0400

    test_netcdf_read edits

--- test_netcdf_read.py
+++ test_netcdf_read.py
@@ -4,17 +4,25 @@
 import netCDF4
 import numpy
 import pylab
-file=netCDF4.Dataset('test_dataset_unlimited.nc')
-xval = file.variables['x'][:]
-yval = file.variables['y'][:]
-tval = file.variables['t'][:]
-data = file.variables['data'][:]
+pylab.ion() 
+file=netCDF4.Dataset('temperature_time_record.nc')
+xval = file.variables['x']
+yval = file.variables['y']
+tval = file.variables['t']
+data = file.variables['temperature']
 print 'shape xval, yval, data',numpy.shape(xval),numpy.shape(yval),numpy.shape(data)
 pylab.figure(1)
 pylab.subplot(2,1,1)
-pylab.plot(xval,data[2,:],'r')
-pylab.plot(xval,data[0,:],'b')
+pylab.plot(xval,data[0,2,:],'r')
+pylab.plot(xval,data[1,2,:],'b')
+pylab.legend(('time %3.1f s'%tval[0],'time %3.1f'%tval[1]))
+pylab.title('temperature for y=%3.1f %s'%(yval[2],yval.units))
+pylab.xlabel('x(%s)'%xval.units)
+pylab.ylabel('t(%s)'%tval.units)
 pylab.subplot(2,1,2)
-pylab.contourf(xval,yval,pylab.squeeze(data[2,:,:]))
+pylab.contourf(xval,yval,data[2,:,:])
+pylab.title('Temperature(%s) at time %3.1f %s'%(data.units,tval[2],tval.units))
+pylab.xlabel('x(%s)'%xval.units)
+pylab.ylabel('y(%s)'%yval.units)
 pylab.colorbar()
-show()
+pylab.show()
